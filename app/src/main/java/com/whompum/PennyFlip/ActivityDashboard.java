652065a2dfbdd9b90562ded193cf97e74ee51bcd
bryan <bryanmills86@yahoo.com>
2017-12-16 20:52:08 -0800

    Initial code commit. Now our journey begins

---
+++ app/src/main/java/com/whompum/PennyFlip/ActivityDashboard.java
@@ -0,0 +1,13 @@
+package com.whompum.PennyFlip;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityDashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboard);
+    }
+}
