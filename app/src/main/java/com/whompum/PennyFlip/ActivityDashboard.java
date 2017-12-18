af919f39854b9067ac531b44feb173ee182f2770
bryan <bryanmills86@yahoo.com>
2017-12-17 21:04:11 -0800

    Add onClick references in code

--- app/src/main/java/com/whompum/PennyFlip/ActivityDashboard.java
+++ app/src/main/java/com/whompum/PennyFlip/ActivityDashboard.java
@@ -2,12 +2,56 @@ package com.whompum.PennyFlip;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.whompum.pennydialog.dialog.PennyDialog;
+
+import currencyedittext.whompum.com.currencyedittext.CurrencyEditText;
 
 public class ActivityDashboard extends AppCompatActivity {
+
+
+    private RecyclerView transactions;
+    private CurrencyEditText value;
+    private TextView timestamp;
+
+    private PennyDialog pennyDialog;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dashboard);
+
+        pennyDialog = PennyDialog.newInstance(null,null);
+
     }
+
+
+    public void onGoalClicked(final View view){
+    }
+
+    public void onCallibrateClicked(final View view){
+    }
+
+    public void onNavigationFabClicked(final View view){
+    }
+
+    public void onPlusFabClicked(final View view){
+        launchPennyDialog();
+    }
+
+    public void onMinusFabClicked(final View view){
+        launchPennyDialog();
+    }
+
+    private void launchPennyDialog(){
+        if(pennyDialog!=null)
+            pennyDialog.show(getSupportFragmentManager(), PennyDialog.TAG);
+    }
+
 }
