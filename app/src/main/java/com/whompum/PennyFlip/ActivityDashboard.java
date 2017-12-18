0612c20c803d23c772d3763f5f24fdf36fe01040
bryan <bryanmills86@yahoo.com>
2017-12-17 22:25:49 -0800

    Add style bundle to PennyDialog; Wireup views

--- app/src/main/java/com/whompum/PennyFlip/ActivityDashboard.java
+++ app/src/main/java/com/whompum/PennyFlip/ActivityDashboard.java
@@ -1,11 +1,11 @@
 package com.whompum.PennyFlip;
 
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.whompum.pennydialog.dialog.PennyDialog;
@@ -15,12 +15,9 @@ import currencyedittext.whompum.com.currencyedittext.CurrencyEditText;
 public class ActivityDashboard extends AppCompatActivity {
 
 
-    private RecyclerView transactions;
-    private CurrencyEditText value;
-    private TextView timestamp;
-
-    private PennyDialog pennyDialog;
-
+    private RecyclerView transactionsList;
+    private CurrencyEditText dashboardValue;
+    private TextView lastTransactionTimestamp;
 
 
     @Override
@@ -28,22 +25,19 @@ public class ActivityDashboard extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dashboard);
 
-        pennyDialog = PennyDialog.newInstance(null,null);
-        toggleTransactions();
+
+        initViews();
+        toggleNullTransactionImage();
     }
 
-    private void toggleTransactions(){
-
-       // boolean hasTransactedToday = (transactions.getAdapter().getItemCount() > 0); //Un comment when ready
-
-        final ViewGroup nullTransaction = findViewById(R.id.null_transaction_layout);
-
-        if(false)
-            nullTransaction.setVisibility(View.VISIBLE);
-        else
-            nullTransaction.setVisibility(View.INVISIBLE);
-
+    private void initViews(){
+        transactionsList = findViewById(R.id.id_dashboard_transactionlist);
+        dashboardValue = findViewById(R.id.id_dashboard_value);
+        lastTransactionTimestamp = findViewById(R.id.id_dashboard_timestamp);
     }
+
+
+
 
     public void onGoalClicked(final View view){
     }
@@ -51,20 +45,38 @@ public class ActivityDashboard extends AppCompatActivity {
     public void onCallibrateClicked(final View view){
     }
 
-    public void onNavigationFabClicked(final View view){
+    public void onNavFabClicked(final View view){
+
     }
 
     public void onPlusFabClicked(final View view){
-        launchPennyDialog();
-    }
+        final Bundle style = new Bundle();
+        style.putInt(PennyDialog.STYLE_KEY, R.style.AddPennyDialogStyle);
+        launchPennyDialog(style);
+    }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 //hi
 
     public void onMinusFabClicked(final View view){
-        launchPennyDialog();
+        final Bundle style = new Bundle();
+        style.putInt(PennyDialog.STYLE_KEY, R.style.MinusPennyDialogStyle);
+        launchPennyDialog(style);
     }
 
-    private void launchPennyDialog(){
-        if(pennyDialog!=null)
-            pennyDialog.show(getSupportFragmentManager(), PennyDialog.TAG);
+
+    private void toggleNullTransactionImage(){
+
+        // boolean hasTransactedToday = (transactionsList.getAdapter().getItemCount() > 0); //Un comment when ready
+
+        final ViewGroup nullTransaction = findViewById(R.id.id_dashboard_null_transactionlist);
+
+        if(true)
+            nullTransaction.setVisibility(View.VISIBLE);
+        else
+            nullTransaction.setVisibility(View.INVISIBLE);
+
+    }
+    private void launchPennyDialog(@Nullable final Bundle pennyArgs){
+        PennyDialog.newInstance(null, pennyArgs)
+                .show(getSupportFragmentManager(), PennyDialog.TAG);
     }
 
 }
