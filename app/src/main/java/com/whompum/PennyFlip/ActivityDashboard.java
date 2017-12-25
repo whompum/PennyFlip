94a1e4701c1c4917f3782fb585d7c69a5b56f76d
bryan <bryanmills86@yahoo.com>
2017-12-25 14:39:37 -0800

    Changed Dashboard UI; Add new views

--- app/src/main/java/com/whompum/PennyFlip/ActivityDashboard.java
+++ app/src/main/java/com/whompum/PennyFlip/ActivityDashboard.java
@@ -3,19 +3,13 @@ package com.whompum.PennyFlip;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.whompum.PennyFlip.Animations.AnimateScale;
 import com.whompum.PennyFlip.NavMenu.NavMenuAnimator;
-import com.whompum.PennyFlip.Time.Timestamp;
-import com.whompum.PennyFlip.Transaction.TransactionTodayAdapter;
-import com.whompum.PennyFlip.Transaction.Transactions;
 import com.whompum.pennydialog.dialog.PennyDialog;
 
 import currencyedittext.whompum.com.currencyedittext.CurrencyEditText;
@@ -23,9 +17,17 @@ import currencyedittext.whompum.com.currencyedittext.CurrencyEditText;
 public class ActivityDashboard extends AppCompatActivity {
 
 
-    private RecyclerView transactionsList;
-    private CurrencyEditText dashboardValue;
-    private TextView lastTransactionTimestamp;
+    private CurrencyEditText value;
+    private ImageButton goal;
+    private ImageButton callibrate;
+
+    private ViewGroup nullTransactionImage;
+
+    private TextView valueSecondaryLabel;
+    private CurrencyEditText valueSecondary;
+
+    private TextView valueTertiaryLabel;
+    private CurrencyEditText valueTertiary;
 
     private NavMenuAnimator navMenuAnimator;
 
@@ -34,13 +36,8 @@ public class ActivityDashboard extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dashboard);
 
-        transactionsList = findViewById(R.id.id_dashboard_transactionlist);
-        transactionsList.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, true));
-        transactionsList.addOnScrollListener(this.scrollListener); //Binds a scrollListener IMPL
 
-
-        dashboardValue = findViewById(R.id.id_dashboard_value);
-        lastTransactionTimestamp = findViewById(R.id.id_dashboard_timestamp);
+        findViewById(R.id.id_dashboard_value);
 
         findViewById(R.id.id_nav_menu_statistics).setOnClickListener(this.statsListener);
         findViewById(R.id.id_nav_menu_history).setOnClickListener(this.historyListener);
@@ -50,16 +47,11 @@ public class ActivityDashboard extends AppCompatActivity {
         this.navMenuAnimator = new NavMenuAnimator();
         navMenuAnimator.bindMenu( ((LinearLayout)findViewById(R.id.id_nav_menu_layout)) ); //Binds the views to the animator
 
-        toggleNullTransactionImage();
+        findViewById(R.id.id_dashboard_transactions_null_image).setVisibility(View.INVISIBLE);
+
     }
 
 
-    private RecyclerView.OnScrollListener scrollListener = new RecyclerView.OnScrollListener() {
-        @Override
-        public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
-            super.onScrollStateChanged(recyclerView, newState);
-        }
-    };
 
     private final View.OnClickListener anchorListner = new View.OnClickListener() {
         @Override
@@ -96,36 +88,16 @@ public class ActivityDashboard extends AppCompatActivity {
 
 
     public void onPlusFabClicked(final View view){
-        if(transactionsList.getAdapter()==null)
-            transactionsList.setAdapter(new TransactionTodayAdapter(this));
-
-        ((TransactionTodayAdapter)transactionsList.getAdapter()).insert(new Transactions(2782, Transactions.TYPE.ADDED, "test"));
-
-        toggleNullTransactionImage();
-
     }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 //hi
+
+
+
     public void onMinusFabClicked(final View view){
         final Bundle style = new Bundle();
         style.putInt(PennyDialog.STYLE_KEY, R.style.MinusPennyDialogStyle);
         launchPennyDialog(style, null);
     }
 
-
-    private void toggleNullTransactionImage(){
-
-        final ViewGroup nullTransaction = findViewById(R.id.id_dashboard_null_transactionlist);
-
-
-        if(transactionsList.getAdapter() != null)
-            if(transactionsList.getAdapter().getItemCount() > 0)
-                nullTransaction.setVisibility(View.INVISIBLE);
-            else
-                nullTransaction.setVisibility(View.INVISIBLE);
-
-        else
-            nullTransaction.setVisibility(View.VISIBLE);
-
-    }
 
 
     /**
@@ -139,5 +111,22 @@ public class ActivityDashboard extends AppCompatActivity {
         PennyDialog.newInstance(cashChangeListener, pennyArgs)
                 .show(getSupportFragmentManager(), PennyDialog.TAG);
     }
+
+    private class StateAdjust{
+
+        /**
+         * TODO add logic for determining what the state of the users transactions is
+         * Then change things as needed. 
+         */
+
+        public StateAdjust(){
+
+        }
+
+
+
+    }
+
+
 
 }
