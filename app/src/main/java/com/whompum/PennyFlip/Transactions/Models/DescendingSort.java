c8bac338bc818a80d8f8f2f9acf5f471975ce7fd
bryan <bryanmills86@yahoo.com>
2018-07-27 01:19:25 -0700

    Create DESC sort order for transactions

---
+++ app/src/main/java/com/whompum/PennyFlip/Transactions/Models/DescendingSort.java
@@ -0,0 +1,15 @@
+package com.whompum.PennyFlip.Transactions.Models;
+
+
+
+import com.whompum.PennyFlip.Money.Transaction.Transaction;
+
+import java.util.Comparator;
+
+public class DescendingSort implements Comparator<Transaction> {
+    @Override
+    public int compare(Transaction o1, Transaction o2) {
+        return (int)(o2.getTimestamp() - o1.getTimestamp()); //If i get an overflow, convert Millis to seconds.
+    }
+
+}
