3600caa76e0898388ef1cdcf31bb14b91e9136dc
bryan <bryanmills86@yahoo.com>
2018-07-27 01:28:23 -0700

    Change query logic for timeranges; Use LiveData<T> as return type

--- app/src/main/java/com/whompum/PennyFlip/Money/Transaction/TransactionDao.java
+++ app/src/main/java/com/whompum/PennyFlip/Money/Transaction/TransactionDao.java
@@ -17,23 +17,23 @@ public interface  TransactionDao {
 
     //ID
     @Query("SELECT * FROM `Transaction` WHERE sourceId = :sourceId")
-    List<Transaction> fetch(@NonNull final String sourceId);
+    LiveData<List<Transaction>> fetch(@NonNull final String sourceId);
 
     //TransactionType
     @Query("SELECT * From `Transaction` WHERE transactionType = :transactionType")
     LiveData<List<Transaction>> fetch(final int transactionType);
 
     //Timestamp
-    @Query("SELECT * From `Transaction` WHERE timestamp <= :floor AND timestamp >= :ciel")
-    List<Transaction> fetch(final long floor, final long ciel);
+    @Query("SELECT * From `Transaction` WHERE timestamp >= :floor AND timestamp <= :ciel")
+    LiveData<List<Transaction>> fetch(final long floor, final long ciel);
 
     //TransactionType - Timestamp
-    @Query("SELECT * FROM `Transaction` WHERE transactionType = :transactionType AND timestamp <= :floor AND timestamp >= :ciel")
-    List<Transaction> fetch(final int transactionType, final long floor, final long ciel);
+    @Query("SELECT * FROM `Transaction` WHERE transactionType = :transactionType AND timestamp >= :floor AND timestamp <= :ciel")
+    LiveData<List<Transaction>> fetch(final int transactionType, final long floor, final long ciel);
 
     //Timestamp - ID
-    @Query("SELECT * From `Transaction` WHERE timestamp <= :floor AND timestamp >= :ciel AND sourceId = :sourceId")
-    List<Transaction> fetch(@NonNull final String sourceId, final long floor, final long ciel);
+    @Query("SELECT * From `Transaction` WHERE timestamp >= :floor AND timestamp <= :ciel AND sourceId = :sourceId")
+    LiveData<List<Transaction>> fetch(@NonNull final String sourceId, final long floor, final long ciel);
 
 
     @Query("DELETE FROM `Transaction`  WHERE sourceId = :sourceId")
