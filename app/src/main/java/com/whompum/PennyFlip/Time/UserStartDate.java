bb49fb1342d8fc982b18c96f9b1ffc7a121da83a
bryan <bryanmills86@yahoo.com>
2018-08-15 16:49:50 -0700

    Route yearly active lookups to UserStartDate

--- app/src/main/java/com/whompum/PennyFlip/Time/UserStartDate.java
+++ app/src/main/java/com/whompum/PennyFlip/Time/UserStartDate.java
@@ -2,6 +2,12 @@ package com.whompum.PennyFlip.Time;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 /**
  * Created by bryan on 2/1/2018.
@@ -46,5 +52,32 @@ public class UserStartDate {
        return context.getSharedPreferences(START_DATE_FILE, Context.MODE_PRIVATE);
     }
 
+    public static List<String> getActiveYears(@NonNull final Context ctx){
+        final List<String> years = new ArrayList<>();
+
+        final int now = Timestamp.now().getYear();
+
+        years.add(String.valueOf(now));
+
+        final int startDateYear = Timestamp.from(
+                UserStartDate.getUserStartDate(ctx)
+        ).getYear();
+
+
+        if(startDateYear != now){
+
+            for(int year = startDateYear; year < now; year++)
+                years.add(String.valueOf(year));
+        }
+
+        Collections.sort(years, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return Integer.valueOf(o1) - Integer.valueOf(o2);
+            }
+        });
+
+        return years; //Return all available years the user has been active
+    }
 
 }
