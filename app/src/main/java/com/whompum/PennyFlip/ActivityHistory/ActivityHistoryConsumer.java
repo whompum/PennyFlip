cf63eb92045babe687943356295c24d1798fac41
Bryan <whompum101@gmail.com>
2018-10-25 02:57:02 -0400

    Create Javadoc

--- app/src/main/java/com/whompum/PennyFlip/ActivityHistory/ActivityHistoryConsumer.java
+++ app/src/main/java/com/whompum/PennyFlip/ActivityHistory/ActivityHistoryConsumer.java
@@ -4,7 +4,13 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 
 import com.whompum.PennyFlip.Money.TimeRange;
+import com.whompum.PennyFlip.Money.Transaction.Transaction;
 
+import java.util.List;
+
+/**
+ * Consumer of data operations done for the {@link ActivityHistory}
+ */
 public interface ActivityHistoryConsumer {
     void fetch(@NonNull final TimeRange timeRange);
     long fetchStartDate(@NonNull final Context c);
--- app/src/main/java/com/whompum/PennyFlip/Money/Transaction/DescendingSort.java
+++ app/src/main/java/com/whompum/PennyFlip/Money/Transaction/DescendingSort.java
@@ -2,6 +2,9 @@ package com.whompum.PennyFlip.Money.Transaction;
 
 import java.util.Comparator;
 
+/**
+ * Sorts a Transaction Object in a Descending order based on its timestamp
+ */
 public class DescendingSort implements Comparator<Transaction> {
     @Override
     public int compare(Transaction o1, Transaction o2) {
