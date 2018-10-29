6f93b9fde1849c95c7e0dd1bc80ff2dad257887b
Bryan <whompum101@gmail.com>
2018-10-29 16:33:07 -0400

    Refractor to new data provider architecture

--- app/src/main/java/com/whompum/PennyFlip/ActivityHistory/HistoryController.java
+++ app/src/main/java/com/whompum/PennyFlip/ActivityHistory/HistoryController.java
@@ -1,80 +1,48 @@
 package com.whompum.PennyFlip.ActivityHistory;
 
-import android.arch.persistence.room.Room;
 import android.content.Context;
-import android.os.Handler;
-import android.os.Message;
 import android.support.annotation.NonNull;
-import android.util.Log;
 
-import com.whompum.PennyFlip.Money.MoneyDatabase;
+import com.whompum.PennyFlip.Money.Queries.Query.MoneyQuery;
+import com.whompum.PennyFlip.Money.Queries.Query.QueryParameter;
+import com.whompum.PennyFlip.Money.Queries.Query.QueryResult;
+import com.whompum.PennyFlip.Money.Queries.QueryReceiver;
 import com.whompum.PennyFlip.Money.TimeRange;
 import com.whompum.PennyFlip.Money.Transaction.DescendingSort;
+import com.whompum.PennyFlip.Money.Transaction.RoomTransactionReader;
 import com.whompum.PennyFlip.Money.Transaction.Transaction;
-import com.whompum.PennyFlip.Money.Transaction.TransactionsDao;
-import com.whompum.PennyFlip.Time.Timestamp;
+import com.whompum.PennyFlip.Money.Transaction.TransactionQueryKeys;
 import com.whompum.PennyFlip.Time.UserStartDate;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
 /**
- * Data provider object for the History Activity.
+ * Exposes a historical view of transaction data, according to a {@link TimeRange},
+ * to a {@link ActivityHistoryClient}.
  */
-public class HistoryController implements ActivityHistoryConsumer, Handler.Callback{
+public class HistoryController implements ActivityHistoryConsumer, QueryReceiver<Collection<Transaction>> {
 
-    private TransactionsDao accessor;
-    private Handler queryHandler = new Handler(this);
+    private RoomTransactionReader reader;
     private ActivityHistoryClient client;
 
-
     public HistoryController(@NonNull final Context context, @NonNull final ActivityHistoryClient client){
-        this.accessor = Room.databaseBuilder(context, MoneyDatabase.class, MoneyDatabase.NAME)
-                .build()
-                .getTransactionAccessor();
+        reader = new RoomTransactionReader(context, this, null);
+
         this.client = client;
     }
 
-    /**
-     * Callback invoked when a query ran and data was received.
-     * This method de
-     * @param msg
-     * @return
-     */
-    @Override
-    public boolean handleMessage(Message msg) {
-        Log.i("TRANSACTION_SELECTION", "handleMessage()");
-        Log.i("TRANSACTION_SELECTION", "is msg.obj null: " + (msg.obj == null) );
-
-        if(!(msg.obj instanceof List)) return true;
-
-        final List<Transaction> transactions = (List<Transaction>) msg.obj;
-
-        onPostQuery( transactions );
-
-        return true;
-    }
 
     @Override
     public void fetch(@NonNull final TimeRange range){
-        //Query LocalMoneyProvider for all Transactions within timerange
+        //QueryParameter LocalMoneyProvider for all Transactions within timerange
 
-        new Thread(){
-            @Override
-            public void run() {
-               List<Transaction> data;
-
-               if( ((data = accessor.fetch(range.getMillisFloor(), range.getMillisCiel())) != null) &&
-                       queryHandler != null) {
-
-                   final Message m = Message.obtain(queryHandler);
-                    m.obj = data;
-
-                    queryHandler.sendMessage(m);
-
-               }
-            }
-        }.start();
+        reader.query(new MoneyQuery.QueryBuilder(TransactionQueryKeys.KEYS)
+                .setQueryParameter(TransactionQueryKeys.TIMERANGE, new QueryParameter<>(range))
+                .getQuery()
+        );
 
     }
 
@@ -90,20 +58,15 @@ public class HistoryController implements ActivityHistoryConsumer, Handler.Callb
     return 1514793600000L;
     }
 
-    /**
-     * Called after a query ran successfully. Prepares the data for the client
-     * and delivers
-     * @param data
-     */
-    private void onPostQuery(@NonNull final List<Transaction> data){
+    @Override
+    public void onQueryReceived(@NonNull QueryResult<Collection<Transaction>> t) {
 
-        //Sort the transaction data
-        Collections.sort( data, new DescendingSort());
+        final List<Transaction> dataSet = new ArrayList<>(t.getT());
+
+        Collections.sort(dataSet, new DescendingSort());
 
         if(client != null)
-            client.onDataQueried(data);
-
+            client.onDataQueried(dataSet);
     }
-
 
 }
