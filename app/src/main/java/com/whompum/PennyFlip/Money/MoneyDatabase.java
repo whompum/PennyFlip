a78a1406c616fcd9611ba1084e5b3482e78cb78d
Bryan <whompum101@gmail.com>
2018-10-29 14:34:43 -0400

    Update to new MoneyDatabase

--- app/src/main/java/com/whompum/PennyFlip/Money/MoneyDatabase.java
+++ app/src/main/java/com/whompum/PennyFlip/Money/MoneyDatabase.java
@@ -6,19 +6,18 @@ import android.arch.persistence.room.RoomDatabase;
 import com.whompum.PennyFlip.Money.Source.ObservableSourceAccessor;
 import com.whompum.PennyFlip.Money.Source.Source;
 import com.whompum.PennyFlip.Money.Source.SourceDao;
+import com.whompum.PennyFlip.Money.Transaction.TransactionDAO;
 import com.whompum.PennyFlip.Money.Transaction.Transaction;
-import com.whompum.PennyFlip.Money.Transaction.TransactionsDao;
 import com.whompum.PennyFlip.Money.Wallet.Wallet;
 import com.whompum.PennyFlip.Money.Wallet.WalletDao;
 
 @Database(entities = {Source.class, Transaction.class, Wallet.class}, version = 1, exportSchema = false)
 public abstract class MoneyDatabase extends RoomDatabase {
-
     public static final String NAME = "money.db";
 
     public abstract SourceDao getSourceAccessor();
     public abstract ObservableSourceAccessor getObservableSourceAccessor();
 
-    public abstract TransactionsDao getTransactionAccessor();
+    public abstract TransactionDAO getTransactionAccessor();
     public abstract WalletDao getWalletAccessor();
 }
