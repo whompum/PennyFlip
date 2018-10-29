33aab91bcfedab2bdfe940b6ffbe42766579f454
Bryan <whompum101@gmail.com>
2018-10-29 14:33:14 -0400

    Create utility worker thread

---
+++ app/src/main/java/com/whompum/PennyFlip/Money/MoneyThread.java
@@ -0,0 +1,26 @@
+package com.whompum.PennyFlip.Money;
+
+import android.support.annotation.NonNull;
+
+public class MoneyThread extends Thread {
+
+    private MoneyThreadOperation moneyThreadOperation;
+
+    @Override
+    public void run() {
+
+        if(moneyThreadOperation != null)
+            moneyThreadOperation.doOperation();
+
+    }
+
+    public void doInBackground(@NonNull final MoneyThreadOperation moneyThreadOperation){
+        this.moneyThreadOperation = moneyThreadOperation;
+        this.start();
+    }
+
+    public static abstract class MoneyThreadOperation {
+       public abstract void doOperation();
+    }
+
+}
