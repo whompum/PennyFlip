977597300c31ac83bbd52835f25ebeed3d639c76
Bryan <whompum101@gmail.com>
2018-10-29 14:28:13 -0400

    Create transaction query files

---
+++ app/src/main/java/com/whompum/PennyFlip/Money/Transaction/RoomTransactionReader.java
@@ -0,0 +1,61 @@
+package com.whompum.PennyFlip.Money.Transaction;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.whompum.PennyFlip.Money.Queries.MoneyDao;
+import com.whompum.PennyFlip.Money.Queries.Query.MoneyQuery;
+import com.whompum.PennyFlip.Money.Queries.QueryReceiver;
+import com.whompum.PennyFlip.Money.Queries.RoomMoneyReader;
+import com.whompum.PennyFlip.Money.TESTMoneyDatabase;
+
+import java.util.Collection;
+
+public class RoomTransactionReader extends RoomMoneyReader<Transaction, Integer> {
+
+
+    public RoomTransactionReader(@NonNull Context context,
+                                 @Nullable QueryReceiver<Collection<Transaction>> groupReceiver,
+                                 @Nullable QueryReceiver<Transaction> itemReceiver) {
+
+        super(context, groupReceiver, itemReceiver);
+    }
+
+
+    @Override
+    public void call(@NonNull MoneyQuery query) {
+
+        //Give the super-class a chance to handle the query in case of defaults or ID
+        super.call(query);
+
+        /*
+            TODO: Map query keys to DAO methods
+            First account for simple cases (default - id)
+            Then start from largest DAO methods to the smallest DAO methods
+            This will be accomplished using boolean flags to determine if we have data
+         */
+
+    }
+
+    @Override
+    protected MoneyDao<Transaction, Integer> getDao(@NonNull TESTMoneyDatabase database) {
+        return database.getTransactionAccessor();
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/whompum/PennyFlip/Money/Transaction/TransactionQueryKeys.java
@@ -0,0 +1,27 @@
+package com.whompum.PennyFlip.Money.Transaction;
+
+import java.util.HashSet;
+
+/**
+ * {@link Transaction} parameter key class
+ * That allows the user to choose one of the following keys
+ */
+public class TransactionQueryKeys{
+
+    public static final int TIMERANGE = 1;
+    public static final int SOURCE_ID = 2;
+    public static final int PENNY_VALUE = 3;
+    public static final int TRANSACTION_TYPE = 4;
+
+    public static final HashSet<Integer> KEYS = new HashSet<>();
+
+    static{
+
+        KEYS.add(TIMERANGE);
+        KEYS.add(SOURCE_ID);
+        KEYS.add(PENNY_VALUE);
+        KEYS.add(TRANSACTION_TYPE);
+
+    }
+
+}
