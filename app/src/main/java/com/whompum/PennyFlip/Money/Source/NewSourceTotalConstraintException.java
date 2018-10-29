599f129d238cbce4a33bf9406b6ce334697368db
Bryan <whompum101@gmail.com>
2018-10-29 14:26:59 -0400

    Add source constraint

--- app/src/main/java/com/whompum/PennyFlip/Money/Source/NewSourceTotalConstraintException.java
+++ app/src/main/java/com/whompum/PennyFlip/Money/Source/NewSourceTotalConstraintException.java
@@ -2,6 +2,6 @@ package com.whompum.PennyFlip.Money.Source;
 
 public class NewSourceTotalConstraintException extends RuntimeException {
     public NewSourceTotalConstraintException(){
-        super("A new source object must have a total equal to zero");
+        super("DaoQueryAdapter new source object must have a total equal to zero");
     }
 }
