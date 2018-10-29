65536a35076722cdf8e710e873f04c571f19e89c
Bryan <whompum101@gmail.com>
2018-10-29 14:31:28 -0400

    Extend MoneyDao for new query archtecture

--- app/src/main/java/com/whompum/PennyFlip/Money/Wallet/WalletDao.java
+++ app/src/main/java/com/whompum/PennyFlip/Money/Wallet/WalletDao.java
@@ -7,14 +7,25 @@ import android.arch.persistence.room.OnConflictStrategy;
 import android.arch.persistence.room.Query;
 import android.support.annotation.NonNull;
 
-import com.whompum.PennyFlip.Money.Wallet.Wallet;
+import com.whompum.PennyFlip.Money.Queries.MoneyDao;
+
+import java.util.List;
 
 @Dao
-public interface WalletDao {
+public interface WalletDao extends MoneyDao<Wallet, Integer> {
+
+    @Override
+    @Query("SELECT * FROM Wallet")
+    List<Wallet> fetchAll();
+
+    @Override
+    @Query("SELECT * FROM Wallet WHERE id = :id")
+    Wallet fetchById(@NonNull final Integer id);
 
     @Query("SELECT * From Wallet")
     LiveData<Wallet> fetchObservable();
 
+    @Deprecated
     @Query("SELECT * FROM Wallet")
     Wallet fetch();
 
