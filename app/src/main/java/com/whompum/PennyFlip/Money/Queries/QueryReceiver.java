67236de848f3e9f5e3a75e4bd8e1ae33b9e9c0b4
Bryan <whompum101@gmail.com>
2018-10-30 14:50:33 -0400

    Allow null data responses

--- app/src/main/java/com/whompum/PennyFlip/Money/Queries/QueryReceiver.java
+++ app/src/main/java/com/whompum/PennyFlip/Money/Queries/QueryReceiver.java
@@ -1,9 +1,10 @@
 package com.whompum.PennyFlip.Money.Queries;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import com.whompum.PennyFlip.Money.Queries.Query.QueryResult;
 
 public interface QueryReceiver<T> {
-    void onQueryReceived(@NonNull final QueryResult<T> t);
+    void onQueryReceived(@Nullable final QueryResult<T> t);
 }
