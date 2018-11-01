040e2927c7b6a7e16a12fc9d3c3380bd7daa9f27
Bryan <whompum101@gmail.com>
2018-11-01 13:51:44 -0400

    Update contracts to return all possible Daos

--- app/src/main/java/com/whompum/PennyFlip/Money/MoneyDatabase.java
+++ app/src/main/java/com/whompum/PennyFlip/Money/MoneyDatabase.java
@@ -1,25 +1,30 @@
 package com.whompum.PennyFlip.Money;
 
+import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.RoomDatabase;
 
-import com.whompum.PennyFlip.Money.Contracts.Source.ObservableSourceAccessor;
+import com.whompum.PennyFlip.Money.Contracts.Source.GenericFetchSourceById;
 import com.whompum.PennyFlip.Money.Contracts.Transaction.TransactionDao;
 import com.whompum.PennyFlip.Money.Source.Source;
 import com.whompum.PennyFlip.Money.Contracts.Source.SourceDao;
-import com.whompum.PennyFlip.Money.Contracts.Transaction.TransactionQueryContractObservable;
 import com.whompum.PennyFlip.Money.Transaction.Transaction;
 import com.whompum.PennyFlip.Money.Wallet.Wallet;
-import com.whompum.PennyFlip.Money.Wallet.WalletDao;
+import com.whompum.PennyFlip.Money.Contracts.Wallet.WalletDao;
+
+import java.util.List;
 
 @Database(entities = {Source.class, Transaction.class, Wallet.class}, version = 1, exportSchema = false)
 public abstract class MoneyDatabase extends RoomDatabase {
     public static final String NAME = "money.db";
 
-    public abstract SourceDao getSourceAccessor();
-    public abstract ObservableSourceAccessor getObservableSourceAccessor();
+    public abstract SourceDao<List<Source>> getSourceAccessor();
+    public abstract GenericFetchSourceById<Source> getSingleSourceAccessor();
+    public abstract GenericFetchSourceById<LiveData<Source>> getObservableSingleSourceAccessor();
+    public abstract SourceDao<LiveData<List<Source>>> getObservableSourceAccessor();
 
-    public abstract TransactionDao getTransactionAccessor();
-    public abstract TransactionQueryContractObservable getObservableTransactionAccessor();
+    public abstract TransactionDao<List<Transaction>> getTransactionAccessor();
+    public abstract TransactionDao<LiveData<List<Transaction>>> getObservableTransactionAccessor();
+
     public abstract WalletDao getWalletAccessor();
 }
