08ea054c8b7da8bc2181f36b3be46aec7feb7d8d
Bryan <whompum101@gmail.com>
2018-11-01 13:57:00 -0400

    Add query failure callback

--- app/src/main/java/com/whompum/PennyFlip/Money/Queries/QueryReceiver.java
+++ app/src/main/java/com/whompum/PennyFlip/Money/Queries/QueryReceiver.java
@@ -1,10 +1,10 @@
 package com.whompum.PennyFlip.Money.Queries;
 
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 import com.whompum.PennyFlip.Money.Queries.Query.QueryResult;
 
 public interface QueryReceiver<T> {
     void onQueryReceived(@Nullable final QueryResult<T> t);
+    void onQueryFailed(final int msg, @Nullable final String reason);
 }
