f09f01b30e40690348870d7d4807607e828cefee
Bryan <whompum101@gmail.com>
2018-11-01 13:38:14 -0400

    Mv to */Money/Contracts/

--- app/src/main/java/com/whompum/PennyFlip/Money/Contracts/Transaction/TransactionDao.java
+++ app/src/main/java/com/whompum/PennyFlip/Money/Contracts/Transaction/TransactionDao.java
@@ -1,9 +1,18 @@
 package com.whompum.PennyFlip.Money.Contracts.Transaction;
 
 import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Query;
+import android.support.annotation.NonNull;
 
+import com.whompum.PennyFlip.Money.Contracts.MoneyDaoId;
 import com.whompum.PennyFlip.Money.Transaction.Transaction;
 import com.whompum.PennyFlip.Money.Contracts.MoneyDaoWriter;
 
 @Dao
-public interface TransactionDao extends TransactionQueryContract, MoneyDaoWriter<Transaction>{}
+public interface TransactionDao<T> extends TransactionQueryDao<T>,
+        MoneyDaoId<Transaction>, MoneyDaoWriter<Transaction>{
+
+    @Override
+    @Query("SELECT * FROM `Transaction` WHERE id = :id")
+    <P> Transaction fetchById(@NonNull final P id);
+}
