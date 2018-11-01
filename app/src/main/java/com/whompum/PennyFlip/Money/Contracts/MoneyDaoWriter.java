041adf04aa44760a70e2f746e21ebc79e8b30b5b
Bryan <whompum101@gmail.com>
2018-11-01 13:52:40 -0400

    Convert to a Room Dao

--- app/src/main/java/com/whompum/PennyFlip/Money/Contracts/MoneyDaoWriter.java
+++ app/src/main/java/com/whompum/PennyFlip/Money/Contracts/MoneyDaoWriter.java
@@ -1,5 +1,7 @@
 package com.whompum.PennyFlip.Money.Contracts;
 
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
 import android.support.annotation.NonNull;
 
 /**
@@ -7,10 +9,12 @@ import android.support.annotation.NonNull;
  * of data
  * @param <T> The type of data we're saving
  */
+@Dao
 public interface MoneyDaoWriter<T> {
     /**
      * Insert a new record into a database.
      * @param t the data to insert
      */
+    @Insert
     void insert(@NonNull final T t);
 }
