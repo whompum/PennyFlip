7de0fb65bad21c25b19f4793eb6de55d76ecc9fe
Bryan <whompum101@gmail.com>
2018-11-01 19:52:40 -0400

    Add Dao for accessing a Transaction by its Id

--- app/src/main/java/com/whompum/PennyFlip/Money/MoneyDatabase.java
+++ app/src/main/java/com/whompum/PennyFlip/Money/MoneyDatabase.java
@@ -6,6 +6,7 @@ import android.arch.persistence.room.RoomDatabase;
 
 import com.whompum.PennyFlip.Money.Contracts.Source.GenericFetchSourceById;
 import com.whompum.PennyFlip.Money.Contracts.Transaction.TransactionDao;
+import com.whompum.PennyFlip.Money.Contracts.Transaction.TransactionIdDao;
 import com.whompum.PennyFlip.Money.Source.Source;
 import com.whompum.PennyFlip.Money.Contracts.Source.SourceDao;
 import com.whompum.PennyFlip.Money.Transaction.Transaction;
@@ -24,6 +25,7 @@ public abstract class MoneyDatabase extends RoomDatabase {
     public abstract SourceDao<LiveData<List<Source>>> getObservableSourceAccessor();
 
     public abstract TransactionDao<List<Transaction>> getTransactionAccessor();
+    public abstract TransactionIdDao getSingleTransactionAccessor();
     public abstract TransactionDao<LiveData<List<Transaction>>> getObservableTransactionAccessor();
 
     public abstract WalletDao getWalletAccessor();
