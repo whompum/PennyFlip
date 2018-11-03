60d03214896040dd1bbcf1b6b5840025aae262b5
Bryan <whompum101@gmail.com>
2018-11-03 17:26:30 -0400

    Update whitespaces

--- app/src/main/java/com/whompum/PennyFlip/Dashboard/TodayFragment.java
+++ app/src/main/java/com/whompum/PennyFlip/Dashboard/TodayFragment.java
@@ -38,9 +38,6 @@ import butterknife.ButterKnife;
 import butterknife.Unbinder;
 import currencyedittext.whompum.com.currencyedittext.CurrencyEditText;
 
-/**
- * Created by bryan on 1/12/2018.
- */
 
 public class TodayFragment extends Fragment implements Observer<List<Transaction>>{
 
@@ -67,28 +64,31 @@ public class TodayFragment extends Fragment implements Observer<List<Transaction
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate( savedInstanceState );
         observeTransactions();
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        final View layout = inflater.inflate(LAYOUT, container, false);
+        final View layout = inflater.inflate( LAYOUT, container, false );
 
-        unbinder = ButterKnife.bind(this, layout);
+        unbinder = ButterKnife.bind( this, layout );
 
-        this.transactionsList.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
-        this.transactionsAdapter = new TodayTransactionAdapter(getContext());
+        this.transactionsList.setLayoutManager( new LinearLayoutManager( getContext(),
+                LinearLayoutManager.HORIZONTAL,
+                false ) );
 
-        transactionsList.setAdapter(transactionsAdapter);
+        this.transactionsAdapter = new TodayTransactionAdapter( getContext() );
 
-        value.setTextColor(getColor(VALUE_TEXT_COLOR));
+        transactionsList.setAdapter( transactionsAdapter );
 
-        //TRIFECTA of touch handling!
-        layout.post(delegate); //Post a runnable for to be ran AFTER the the layout passes.
-        layout.setOnTouchListener(delegateListener);
-        transactionsList.requestDisallowInterceptTouchEvent(true);
+        value.setTextColor( getColor( VALUE_TEXT_COLOR ) );
+
+        //Trifecta approach to increasing the lists draggable bounds
+        layout.post( delegate ); //Post a runnable for to be ran AFTER the the layout passes.
+        layout.setOnTouchListener( delegateListener );
+        transactionsList.requestDisallowInterceptTouchEvent( true );
 
     return layout;
     }
@@ -103,9 +103,9 @@ public class TodayFragment extends Fragment implements Observer<List<Transaction
     @Override
     public void onChanged(@Nullable List<Transaction> transactions) {
 
-        if(transactions == null || transactionsAdapter == null) return;
+        if( transactions == null || transactionsAdapter == null ) return;
 
-       Collections.sort(transactions, new DescendingSort());
+       Collections.sort( transactions, new DescendingSort() );
 
         /**
          * set anchor at 0, check if any index has a larger value than index 0.
@@ -113,8 +113,8 @@ public class TodayFragment extends Fragment implements Observer<List<Transaction
          * After one full iteration where we found the best anchor to use.
          */
 
-        ((TodayTransactionAdapter)transactionsAdapter).swapDataset(transactions);
-        updateValue(transactions);
+        ((TodayTransactionAdapter)transactionsAdapter).swapDataset( transactions );
+        updateValue( transactions );
     }
 
     private void observeTransactions(){
@@ -130,7 +130,7 @@ public class TodayFragment extends Fragment implements Observer<List<Transaction
         data.attachResponder(new Responder<LiveData<List<Transaction>>>() {
             @Override
             public void onActionResponse(@NonNull LiveData<List<Transaction>> data) {
-                data.observe(TodayFragment.this, TodayFragment.this);
+                data.observe( TodayFragment.this, TodayFragment.this );
             }
         });
     }
@@ -139,9 +139,9 @@ public class TodayFragment extends Fragment implements Observer<List<Transaction
     private TimeRange fetchQueryTimeRange(){
 
         final long today = Timestamp.now().getStartOfDay();
-        final long tomorrow = Timestamp.fromProjection(1).getStartOfDay(); //Manana
+        final long tomorrow = Timestamp.fromProjection( 1 ).getStartOfDay(); //Manana
 
-        return new TimeRange(today, tomorrow);
+        return new TimeRange( today, tomorrow );
     }
 
     /**
@@ -153,48 +153,51 @@ public class TodayFragment extends Fragment implements Observer<List<Transaction
 
         long total = 0L;
 
-        for(Transaction t: transactions)
+        for( Transaction t: transactions )
             total += t.getAmount();
 
-        this.value.setText(String.valueOf(total));
+        this.value.setText( String.valueOf( total ) );
 
     }
 
     protected void setValue(final long pennies){
-        setValue(String.valueOf(pennies));
+        setValue( String.valueOf( pennies ) );
     }
 
     protected void setValue(final String value){
-        this.value.setText(value);
+        this.value.setText( value );
     }
 
     private int getColor(@ColorRes final int colorRes){
 
-        if(Build.VERSION.SDK_INT >= 23)
-            return getContext().getColor(colorRes);
+        if( Build.VERSION.SDK_INT >= 23 )
+            return getContext().getColor( colorRes );
         else
-            return getContext().getResources().getColor(colorRes);
+            return getContext().getResources().getColor( colorRes );
 
     }
 
     private final Runnable delegate = new Runnable() {
         @Override
         public void run() {
-            if(getView() == null)
+            if( getView() == null )
                 return;
-            delegateBoundary.set(getView().getLeft(), transactionsList.getTop(), getView().getRight(), getView().getBottom());
+            delegateBoundary.set( getView().getLeft(),
+                    transactionsList.getTop(),
+                    getView().getRight(),
+                    getView().getBottom() );
         }
     };
 
 
     View.OnTouchListener delegateListener = new View.OnTouchListener() {
         @Override
-        public boolean onTouch(View v, MotionEvent event) {
+        public boolean onTouch( View v, MotionEvent event ) {
 
-            boolean consume = delegateBoundary.contains((int)event.getX(), (int)event.getY());
+            boolean consume = delegateBoundary.contains( (int)event.getX(), (int)event.getY() );
 
-            if(consume)
-                transactionsList.onTouchEvent(event);
+            if( consume )
+                transactionsList.onTouchEvent( event );
 
             return consume;
         }
