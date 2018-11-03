e9650b6d25b64be62f4fb8abd24513f0acf2aadf
Bryan <whompum101@gmail.com>
2018-11-03 00:04:20 -0400

    Create generic QueryHelper utility

---
+++ app/src/main/java/com/whompum/PennyFlip/Money/Queries/QueryHelper.java
@@ -0,0 +1,45 @@
+package com.whompum.PennyFlip.Money.Queries;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.whompum.PennyFlip.Money.Queries.Deliverable;
+import com.whompum.PennyFlip.Money.Queries.Query.MoneyRequest;
+import com.whompum.PennyFlip.Money.Queries.Query.QueryReceiver;
+import com.whompum.PennyFlip.Money.Queries.Query.QueryResult;
+import com.whompum.PennyFlip.Money.Queries.Resolvers.QueryHandler;
+import com.whompum.PennyFlip.Money.Queries.Resolvers.QueryResolver;
+
+public class QueryHelper<T> {
+
+    private Deliverable<T> deliverable;
+    private QueryHandler<T> handler;
+
+    public QueryHelper(QueryResolver<T> resolver) {
+        deliverable = new Deliverable<>();
+
+        handler = new QueryHandler<>( resolver );
+
+        handler.setQueryReceiver(new QueryReceiver<T>() {
+            @Override
+            public void onQueryReceived(@Nullable QueryResult<T> t) {
+                if( t != null && t.getT() != null )
+                    deliverable.setData( t.getT() );
+            }
+
+            @Override
+            public void onQueryFailed(int reason, @Nullable String msg) {
+
+            }
+        });
+
+    }
+
+    public Deliverable<T> query(@NonNull final MoneyRequest request){
+
+        handler.call( request );
+
+        return deliverable;
+    }
+
+}
