22cc16bff883a21a62d9764a6ce9ad1804bffc4e
Bryan <whompum101@gmail.com>
2018-11-03 10:00:37 -0400

    Update to new query pattern

--- app/src/main/java/com/whompum/PennyFlip/ActivityHistory/HistoryController.java
+++ app/src/main/java/com/whompum/PennyFlip/ActivityHistory/HistoryController.java
@@ -3,19 +3,17 @@ package com.whompum.PennyFlip.ActivityHistory;
 import android.content.Context;
 import android.support.annotation.NonNull;
 
-import com.whompum.PennyFlip.Money.Queries.Query.MoneyQuery;
-import com.whompum.PennyFlip.Money.Queries.Query.QueryParameter;
-import com.whompum.PennyFlip.Money.Queries.Query.QueryResult;
-import com.whompum.PennyFlip.Money.Queries.QueryReceiver;
+import com.whompum.PennyFlip.Money.DatabaseUtils;
+import com.whompum.PennyFlip.Money.MoneyDatabase;
+import com.whompum.PennyFlip.Money.Queries.Responder;
+import com.whompum.PennyFlip.Money.Queries.TransactionQueries;
 import com.whompum.PennyFlip.Money.TimeRange;
 import com.whompum.PennyFlip.Money.Transaction.DescendingSort;
-import com.whompum.PennyFlip.Money.Transaction.RoomTransactionReader;
 import com.whompum.PennyFlip.Money.Transaction.Transaction;
+import com.whompum.PennyFlip.Money.Transaction.TransactionQueryBuilder;
 import com.whompum.PennyFlip.Money.Transaction.TransactionQueryKeys;
 import com.whompum.PennyFlip.Time.UserStartDate;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
@@ -23,26 +21,45 @@ import java.util.List;
  * Exposes a historical view of transaction data, according to a {@link TimeRange},
  * to a {@link ActivityHistoryClient}.
  */
-public class HistoryController implements ActivityHistoryConsumer, QueryReceiver<Collection<Transaction>> {
+public class HistoryController implements ActivityHistoryConsumer {
 
-    private RoomTransactionReader reader;
     private ActivityHistoryClient client;
 
-    public HistoryController(@NonNull final Context context, @NonNull final ActivityHistoryClient client){
-        reader = new RoomTransactionReader(context, this, null);
+    private TransactionQueryBuilder builder = new TransactionQueryBuilder();
 
+    private TransactionQueries server = new TransactionQueries();
+
+    private MoneyDatabase database;
+
+    public HistoryController(@NonNull final Context context, @NonNull final ActivityHistoryClient client){
         this.client = client;
+        database = DatabaseUtils.getMoneyDatabase( context );
     }
 
 
+    /**
+     * Fetch a list of {@link Transaction} objects according to
+     * the provided {@link TimeRange}
+     *
+     * @param range The minimum and maximum dates to fetch Transactions for
+     */
     @Override
     public void fetch(@NonNull final TimeRange range){
-        //QueryParameter LocalMoneyProvider for all Transactions within timerange
 
-        reader.query(new MoneyQuery.QueryBuilder(TransactionQueryKeys.KEYS)
-                .setQueryParameter(TransactionQueryKeys.TIMERANGE, new QueryParameter<>(range))
-                .getQuery()
-        );
+        builder.setQueryParameter( TransactionQueryKeys.TIMERANGE, range );
+
+        server.queryGroup( builder.getQuery(), database )
+          .attachResponder(new Responder<List<Transaction>>() {
+            @Override
+            public void onActionResponse(@NonNull List<Transaction> data) {
+
+                Collections.sort( data, new DescendingSort() );
+
+                if(client != null)
+                    client.onDataQueried( data );
+
+            }
+        });
 
     }
 
@@ -50,23 +67,12 @@ public class HistoryController implements ActivityHistoryConsumer, QueryReceiver
     public long fetchStartDate(@NonNull final Context context){
         //Fetch start date
 
-       final long userStartDate = UserStartDate.getUserStartDate(context);
+       final long userStartDate = UserStartDate.getUserStartDate( context );
 
        //return UserStartDate.getUserStartDate(context);
        //Swap with user start data when in production.
 
     return 1514793600000L;
-    }
-
-    @Override
-    public void onQueryReceived(@NonNull QueryResult<Collection<Transaction>> t) {
-
-        final List<Transaction> dataSet = new ArrayList<>(t.getT());
-
-        Collections.sort(dataSet, new DescendingSort());
-
-        if(client != null)
-            client.onDataQueried(dataSet);
     }
 
 }
