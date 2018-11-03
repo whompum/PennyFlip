36d67935819d453ab7410a20c79e6ad80c1daac1
Bryan <whompum101@gmail.com>
2018-11-02 23:33:37 -0400

    Add LiveData contract

--- app/src/main/java/com/whompum/PennyFlip/Money/Contracts/Wallet/WalletDao.java
+++ app/src/main/java/com/whompum/PennyFlip/Money/Contracts/Wallet/WalletDao.java
@@ -1,24 +1,23 @@
 package com.whompum.PennyFlip.Money.Contracts.Wallet;
 
+import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.OnConflictStrategy;
 import android.arch.persistence.room.Query;
 import android.support.annotation.NonNull;
 
-import com.whompum.PennyFlip.Money.Contracts.MoneyDaoId;
 import com.whompum.PennyFlip.Money.Wallet.Wallet;
 
 @Dao
-public abstract class WalletDao implements MoneyDaoId<Wallet> {
+public interface WalletDao {
 
-    @Override
-    public <I> Wallet fetchById(@NonNull final I id){
-        return fetchWallet();
-    }
 
     @Query("SELECT * FROM Wallet")
-    public abstract Wallet fetchWallet();
+    Wallet fetchWallet();
+
+    @Query("SELECT * FROM WALLET")
+    LiveData<Wallet> fetchObservableWallet();
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     abstract void update(@NonNull final Wallet wallet);
