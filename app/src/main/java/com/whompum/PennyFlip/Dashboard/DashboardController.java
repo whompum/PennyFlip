07775afb00fa691ab83937a1967b63eea076909c
Bryan <whompum101@gmail.com>
2018-11-03 15:19:59 -0400

    Update queries to new architecture

--- app/src/main/java/com/whompum/PennyFlip/Dashboard/DashboardController.java
+++ app/src/main/java/com/whompum/PennyFlip/Dashboard/DashboardController.java
@@ -1,14 +1,23 @@
 package com.whompum.PennyFlip.Dashboard;
 
 import android.arch.lifecycle.LifecycleOwner;
+import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.Observer;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
-import com.whompum.PennyFlip.Money.MoneyWriter;
-import com.whompum.PennyFlip.Money.RoomMoneyWriter;
+import com.whompum.PennyFlip.Money.DatabaseUtils;
+import com.whompum.PennyFlip.Money.MoneyDatabase;
+import com.whompum.PennyFlip.Money.Queries.Deliverable;
+import com.whompum.PennyFlip.Money.Queries.LoggerResponder;
+import com.whompum.PennyFlip.Money.Queries.Query.MoneyRequest;
+import com.whompum.PennyFlip.Money.Queries.Responder;
+import com.whompum.PennyFlip.Money.Queries.WalletQueries;
+import com.whompum.PennyFlip.Money.Queries.WalletRequestBuilder;
+import com.whompum.PennyFlip.Money.Writes.MoneyWriter;
+import com.whompum.PennyFlip.Money.Writes.RoomMoneyWriter;
 import com.whompum.PennyFlip.Money.Source.NewSourceTotalConstraintException;
 import com.whompum.PennyFlip.Time.UserStartDate;
 import com.whompum.PennyFlip.DialogSourceChooser.SourceWrapper;
@@ -22,7 +31,7 @@ public class DashboardController implements ActivityDashboardConsumer, Observer<
     private DashboardClient client;
 
     private MoneyWriter writer;
-
+    private MoneyDatabase database;
 
     public DashboardController(@NonNull final Context context){
         this(context, null);
@@ -36,10 +45,13 @@ public class DashboardController implements ActivityDashboardConsumer, Observer<
     public DashboardController(@NonNull final Context context, @Nullable LifecycleOwner o){
         UserStartDate.set(context); //Sets the user start date. If already set then it will skip
         //repo = LocalMoneyProvider.obtain(context);
-        this.writer = new RoomMoneyWriter(context);
 
-        if(o != null) {
-            bindWalletObserver(o);
+        this.database = DatabaseUtils.getMoneyDatabase( context );
+
+        this.writer = new RoomMoneyWriter( database );
+
+        if( o != null ) {
+            bindWalletObserver( o );
         }
     }
 
@@ -51,32 +63,41 @@ public class DashboardController implements ActivityDashboardConsumer, Observer<
     @Override
     public void bindWalletObserver(@NonNull final LifecycleOwner o){
         //Fetch an observable wallet, and register the lifecycle owner on it.
+
+        final Deliverable<LiveData<Wallet>> deliverable =
+                new WalletQueries().queryObservable( new WalletRequestBuilder().getQuery(), database );
+
+        deliverable.attachResponder(new Responder<LiveData<Wallet>>() {
+            @Override
+            public void onActionResponse(@NonNull LiveData<Wallet> data) {
+                data.observe( o, DashboardController.this );
+            }
+        });
+
+        deliverable.attachCancelledResponder( new LoggerResponder( DashboardController.class ) );
+
     }
 
     public void saveTransaction(@NonNull final SourceWrapper w, @NonNull final Transaction t){
         //Save the source first, if its new
-        if((w.getTag().equals(SourceWrapper.TAG.NEW))) {
+        if( ( w.getTag().equals( SourceWrapper.TAG.NEW ) ) ) {
 
             try{
-                writer.saveSource(w.getSource());
-            }catch (NewSourceTotalConstraintException e){
-                w.getSource().setPennies(0L);
-                writer.saveSource(w.getSource());
+                writer.saveSource( w.getSource() );
+            }catch ( NewSourceTotalConstraintException e ){
+                w.getSource().setPennies( 0L );
+                writer.saveSource( w.getSource() );
             }
 
         }
-
-        writer.saveTransaction(t);
-
+        writer.saveTransaction( t );
     }
 
     @Override
     public void onChanged(@Nullable Wallet wallet) {
 
-        Log.i("WALLET_FIX", "onChanged(Wallet)#DashboardController Wallet is null: " + (wallet==null) );
-
-        if(wallet != null)
-            if(client != null)
-                client.onWalletChanged(wallet.getValue());
+        if( wallet != null )
+            if( client != null )
+                client.onWalletChanged( wallet.getValue() );
     }
 }
