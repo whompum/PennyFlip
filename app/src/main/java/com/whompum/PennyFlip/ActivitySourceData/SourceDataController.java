e885ef5b07876e0b57357761d351291bbcf43294
Bryan <whompum101@gmail.com>
2018-11-03 15:07:25 -0400

    Update to new query architecture

--- app/src/main/java/com/whompum/PennyFlip/ActivitySourceData/SourceDataController.java
+++ app/src/main/java/com/whompum/PennyFlip/ActivitySourceData/SourceDataController.java
@@ -4,13 +4,19 @@ import android.arch.lifecycle.LifecycleOwner;
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.Observer;
 import android.content.Context;
-import android.os.Handler;
-import android.os.Message;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
-import com.whompum.PennyFlip.Money.MoneyWriter;
-import com.whompum.PennyFlip.Money.RoomMoneyWriter;
+import com.whompum.PennyFlip.Money.DatabaseUtils;
+import com.whompum.PennyFlip.Money.MoneyDatabase;
+import com.whompum.PennyFlip.Money.Queries.Deliverable;
+import com.whompum.PennyFlip.Money.Queries.LoggerResponder;
+import com.whompum.PennyFlip.Money.Queries.Query.MoneyRequest;
+import com.whompum.PennyFlip.Money.Queries.Responder;
+import com.whompum.PennyFlip.Money.Queries.SourceQueries;
+import com.whompum.PennyFlip.Money.Source.SourceQueryKeys;
+import com.whompum.PennyFlip.Money.Writes.MoneyWriter;
+import com.whompum.PennyFlip.Money.Writes.RoomMoneyWriter;
 import com.whompum.PennyFlip.Money.Source.Source;
 import com.whompum.PennyFlip.Money.Transaction.Transaction;
 
@@ -18,39 +24,60 @@ public class SourceDataController  implements SourceDataConsumer, Observer<Sourc
 
     private SourceDataClient client;
 
-    private LiveData<Source> data;
+    private LiveData<Source> sourceObservable;
 
     private MoneyWriter dataWriter;
+    private MoneyRequest.QueryBuilder builder = new MoneyRequest.QueryBuilder(
+            SourceQueryKeys.KEYS
+    );
+
+    private SourceQueries queries = new SourceQueries();
+
+    private MoneyDatabase database;
 
     public SourceDataController(@NonNull final Context context, @NonNull final SourceDataClient client){
         this.client = client;
-        this.dataWriter = new RoomMoneyWriter(context);
+        this.database = DatabaseUtils.getMoneyDatabase( context );
+        this.dataWriter = new RoomMoneyWriter( database );
     }
 
     @Override
     public void observeSource(@NonNull final String sourceId, @NonNull final LifecycleOwner l){
+        builder.setQueryParameter( SourceQueryKeys.TITLE, sourceId );
 
+        final Deliverable<LiveData<Source>> deliverable =
+                queries.queryObservableById( builder.getQuery(), database );
+
+        deliverable.attachResponder(new Responder<LiveData<Source>>() {
+            @Override
+            public void onActionResponse(@NonNull LiveData<Source> data) {
+                sourceObservable = data;
+                data.observe( l, SourceDataController.this );
+            }
+        });
+
+        deliverable.attachCancelledResponder( new LoggerResponder( SourceDataController.class ) );
     }
 
     @Override
     public void saveTransaction(@NonNull Transaction transaction) {
-        dataWriter.saveTransaction(transaction);
+        dataWriter.saveTransaction( transaction );
     }
 
     @Override
     public void deleteSource(@NonNull String sourceId) {
-        dataWriter.deleteSource(sourceId);
+        dataWriter.deleteSource( sourceId );
     }
 
     @Override
     public void unObserve(@NonNull LifecycleOwner owner) {
-        if(data.hasActiveObservers())
-            data.removeObservers(owner);
+        if( sourceObservable.hasActiveObservers() )
+            sourceObservable.removeObservers( owner );
     }
 
     @Override
     public void onChanged(@Nullable Source source) {
-         if(client != null && source != null) client.onSourceChanged(source);
+         if( client != null && source != null ) client.onSourceChanged( source );
     }
 
 }
