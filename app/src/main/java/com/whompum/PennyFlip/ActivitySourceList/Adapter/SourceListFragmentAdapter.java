b7598bf2a7263f9454ba774ee8640598f5a08b98
Bryan <whompum101@gmail.com>
2018-11-05 20:17:26 -0500

    Update constructor to accept an array of fragments

--- app/src/main/java/com/whompum/PennyFlip/ActivitySourceList/Adapter/SourceListFragmentAdapter.java
+++ app/src/main/java/com/whompum/PennyFlip/ActivitySourceList/Adapter/SourceListFragmentAdapter.java
@@ -1,5 +1,6 @@
 package com.whompum.PennyFlip.ActivitySourceList.Adapter;
 
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -14,21 +15,29 @@ import com.whompum.PennyFlip.ActivitySourceList.Fragments.FragmentSourceListSpen
 
 public class SourceListFragmentAdapter extends FragmentPagerAdapter {
 
+    private FragmentSourceList[] fragments = null;
 
-    private final FragmentSourceList addingFragment = FragmentSourceListAdd.newInstance(null);
-    private final FragmentSourceList spendingFragment = FragmentSourceListSpend.newInstance(null);
-
-    public SourceListFragmentAdapter(final FragmentManager fragmentManager){
+    public SourceListFragmentAdapter(final FragmentManager fragmentManager,
+                                     @NonNull final FragmentSourceList... fragments){
         super(fragmentManager);
+
+        this.fragments = fragments;
+
     }
 
     @Override
     public Fragment getItem(int position) {
-        return (position==0) ? addingFragment : spendingFragment;
+
+        if( fragments == null || position > 0 || position < fragments.length-1 ) return null;
+
+        return fragments[ position ];
     }
 
     @Override
     public int getCount() {
-        return 2;
+        if( fragments == null )
+            return 0;
+
+        return fragments.length;
     }
 }
