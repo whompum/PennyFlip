3ef1437e3d66856c62961dd0892a822be9c26668
Bryan <whompum101@gmail.com>
2018-11-09 17:13:49 -0500

    Convert DS from Array to List

--- app/src/main/java/com/whompum/PennyFlip/ActivitySourceList/Adapter/SourceListFragmentAdapter.java
+++ app/src/main/java/com/whompum/PennyFlip/ActivitySourceList/Adapter/SourceListFragmentAdapter.java
@@ -4,10 +4,14 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.view.ViewGroup;
 
 import com.whompum.PennyFlip.ActivitySourceList.Fragments.FragmentSourceList;
-import com.whompum.PennyFlip.ActivitySourceList.Fragments.FragmentSourceListAdd;
-import com.whompum.PennyFlip.ActivitySourceList.Fragments.FragmentSourceListSpend;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by bryan on 12/27/2017.
@@ -15,22 +19,38 @@ import com.whompum.PennyFlip.ActivitySourceList.Fragments.FragmentSourceListSpen
 
 public class SourceListFragmentAdapter extends FragmentPagerAdapter {
 
-    private FragmentSourceList[] fragments = null;
+    private List<FragmentSourceList> fragments = null;
 
     public SourceListFragmentAdapter(final FragmentManager fragmentManager,
                                      @NonNull final FragmentSourceList... fragments){
         super(fragmentManager);
 
-        this.fragments = fragments;
+        this.fragments = Arrays.asList( fragments );
 
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        FragmentSourceList fragment = (FragmentSourceList)
+                super.instantiateItem(container, position);
+
+        fragments.set( position, fragment );
+
+        return fragment;
     }
 
     @Override
     public Fragment getItem(int position) {
 
-        if( fragments == null || position > 0 || position < fragments.length-1 ) return null;
+        if( fragments == null || position < 0 || position > fragments.size()-1 ) return null;
 
-        return fragments[ position ];
+        return fragments.get( position );
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        super.destroyItem(container, position, object);
+        fragments.remove( position );
     }
 
     @Override
@@ -38,6 +58,6 @@ public class SourceListFragmentAdapter extends FragmentPagerAdapter {
         if( fragments == null )
             return 0;
 
-        return fragments.length;
+        return fragments.size();
     }
 }
