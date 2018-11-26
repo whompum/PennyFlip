0177d8be19d4c65d4a8ae918572004aed43c18b9
Bryan <whompum101@gmail.com>
2018-11-26 11:25:31 -0500

    Implement Parcelable interface

--- app/src/main/java/com/whompum/PennyFlip/Money/TimeRange.java
+++ app/src/main/java/com/whompum/PennyFlip/Money/TimeRange.java
@@ -1,8 +1,24 @@
 package com.whompum.PennyFlip.Money;
 
-public class TimeRange {
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.support.annotation.NonNull;
+
+public class TimeRange implements Parcelable {
 
     private long millisFloor, millisCiel;
+
+    public static Creator<TimeRange> CREATOR = new Creator<TimeRange>() {
+        @Override
+        public TimeRange createFromParcel(Parcel source) {
+            return new TimeRange( source );
+        }
+
+        @Override
+        public TimeRange[] newArray(int size) {
+            return new TimeRange[0];
+        }
+    };
 
     public TimeRange(final long m1, final long m2){
         if(m1 >= m2){
@@ -14,7 +30,22 @@ public class TimeRange {
         }
     }
 
+    public TimeRange(@NonNull final Parcel p){
+        this( p.readLong(), p.readLong() );
+    }
+
     public TimeRange() {
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeLong( millisFloor );
+        dest.writeLong( millisCiel );
     }
 
     public long getMillisFloor() {
