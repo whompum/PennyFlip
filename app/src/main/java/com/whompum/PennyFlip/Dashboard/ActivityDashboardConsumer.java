1ed123939401806ccb64c949c24cca4a7d162fa7
Bryan <whompum101@gmail.com>
2018-11-27 21:05:59 -0500

    Rm spaghetti-logic method contracts

--- app/src/main/java/com/whompum/PennyFlip/Dashboard/ActivityDashboardConsumer.java
+++ app/src/main/java/com/whompum/PennyFlip/Dashboard/ActivityDashboardConsumer.java
@@ -1,14 +1,11 @@
 package com.whompum.PennyFlip.Dashboard;
 
-import android.arch.lifecycle.LifecycleOwner;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 
 import com.whompum.PennyFlip.DialogSourceChooser.SourceWrapper;
 import com.whompum.PennyFlip.Money.Transaction.Transaction;
 
 public interface ActivityDashboardConsumer{
     void saveTransaction(@NonNull final SourceWrapper source, @NonNull final Transaction transaction);
-    void bindWalletObserver(@NonNull final LifecycleOwner o);
-    void bindClient(@NonNull final DashboardClient client);
+    long newWalletWithTransaction(@NonNull final Transaction transaction);
 }
