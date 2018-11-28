ca0a08d425c5946df5c4ff3504e0df0ec4c7ee46
Bryan <whompum101@gmail.com>
2018-11-27 21:06:40 -0500

    Expose contracts the Controller needs to handle queries

--- app/src/main/java/com/whompum/PennyFlip/Dashboard/DashboardClient.java
+++ app/src/main/java/com/whompum/PennyFlip/Dashboard/DashboardClient.java
@@ -1,5 +1,16 @@
 package com.whompum.PennyFlip.Dashboard;
 
+import android.arch.lifecycle.LifecycleOwner;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.whompum.PennyFlip.Money.Transaction.Transaction;
+
+import java.util.List;
+
 public interface DashboardClient {
     void onWalletChanged(final long pennies );
+    LifecycleOwner getLifecycleOwner();
+    void handleAddTransactions(@Nullable final List<Transaction> data);
+    void handleSpendTransactions(@Nullable final List<Transaction> data);
 }
