c3446350105fc62c19eaf4811f7e361cafa99f2d
Bryan <whompum101@gmail.com>
2018-12-02 14:29:19 -0500

    Add client contract to receive wallet notification updates

--- app/src/main/java/com/whompum/PennyFlip/ActivitySourceData/SourceDataClient.java
+++ app/src/main/java/com/whompum/PennyFlip/ActivitySourceData/SourceDataClient.java
@@ -6,11 +6,13 @@ import android.support.annotation.TransitionRes;
 
 import com.whompum.PennyFlip.Money.Source.Source;
 import com.whompum.PennyFlip.Money.Transaction.Transaction;
+import com.whompum.PennyFlip.Money.Wallet.Wallet;
 
 import java.util.List;
 
 public interface SourceDataClient {
     void onSourceChanged(@NonNull final Source source);
     void onTransactionChanged(@NonNull final List<Transaction> data);
+    void onWalletChanged(@NonNull final Wallet wallet);
     LifecycleOwner getLifecycleOwner();
 }
