fa227f6c613db865cc41a0ad703a48b7ea7e4203
Bryan <whompum101@gmail.com>
2019-01-10 12:52:16 -0500

    Rm useless class

--- app/src/main/java/com/whompum/PennyFlip/ActivityStatistics/DateQueryable.java
+++
@@ -1,9 +0,0 @@
-package com.whompum.PennyFlip.ActivityStatistics;
-
-import android.support.annotation.Nullable;
-
-import com.whompum.PennyFlip.Money.TimeRange;
-
-public interface DateQueryable {
-    void setDate(@Nullable final TimeRange range);
-}
